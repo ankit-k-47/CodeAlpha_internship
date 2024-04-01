8487f67587a56e6e836c8a9596514f2c9c7b1385
Ankit Kalburgi <162859636+ankit-k-47@users.noreply.github.com>
2024-04-01 15:06:34 +0530

    Update script.js

--- CodeAlpha_portfolio/script.js
+++ CodeAlpha_portfolio/script.js
@@ -1,26 +1,29 @@
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
 
-    let menuIcon = document.querySelector("#menu-icon");
-    let navbar = document.querySelector(".navbar");
-    let sections = document.querySelectorAll("section");
-    let navLinks = document.querySelectorAll("header nav a");
- console.log(menuIcon);
-    window.onscroll = () => {
-        sections.forEach(sec => {
-            let top = window.scrollY;
-            let offset = sec.offsetTop - 150;
-            let height = sec.offsetHeight;
-            let id = sec.getAttribute("id");
-            if (top >= offset && top < offset + height) {
-                navLinks.forEach(link => link.classList.remove("active"));
-                document.querySelector("header nav a[href*=" + id + "]").classList.add("active");
-            }
-        });
-    };
-    menuIcon.onclick = () => {
-        menuIcon.classList.toggle("bx-x");
-        navbar.classList.toggle("active");
-        console.log(navbar.classList);
-    }
+console.log(menuIcon);
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute("id");
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => link.classList.remove("active"));
+            document.querySelector("header nav a[href*=" + id + "]").classList.add("active");
+        }
+    });
+};
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle("bx-x");
+    navbar.classList.toggle("active");
+    console.log(navbar.classList);
+}
+
 const aboutSection = document.querySelector('.about');
 const aboutContent = document.querySelector('.about-content p');
 const serSection = document.querySelector('.services-container');
@@ -42,4 +45,4 @@ observer.observe(aboutSection);
 observer.observe(aboutContent);
 serBoxes.forEach(box => {
     observer.observe(box);
-}); 
+});
