25066a9324edec505dc83439d823569c7003247c
Ankit Kalburgi <162859636+ankit-k-47@users.noreply.github.com>
2024-04-07 13:08:21 +0530

    Social media dashboard

---
+++ CodeAlpha_socialmedia_dashboard/index.js
@@ -0,0 +1,140 @@
+function updf(){
+    let fb=document.getElementById("fb");
+    fb.innerHTML=`<div class="card-header">
+            <img src="images/icon-facebook.svg" alt="fb-icon" />
+            <p>@ronaldosui</p>
+          </div>
+          <div class="card-body">
+            <h1>2900</h1>
+            <p>followers</p>
+          </div>
+          <div class="card-footer">
+            <img src="images/icon-up.svg" alt="up-icon" />
+            <p>34 today</p>
+          </div>`
+    let fbs=document.getElementById("fbs");
+    fbs.innerHTML=`<h3 class="page-views">likes</h3>
+          <img class="fb-icon" src="images/icon-facebook.svg" alt="fb-icon" />
+          <h2>89</h2>
+          <img class="up-down-icon" src="images/icon-down.svg" alt="up" />
+          <p class="percentage fb-down">5%</p>`
+    let fbst=document.getElementById("fbst");
+    fbst.innerHTML=` <h3 class="page-views">views</h3>
+          <img class="fb-icon" src="images/icon-facebook.svg" alt="fb-icon" />
+          <h2>90</h2>
+          <img class="up-down-icon" src="images/icon-up.svg" alt="up" />
+          <p class="percentage fb-up">4%</p>`
+}
+function updaf(){
+    let fb=document.getElementById("fb");
+    fb.innerHTML=`<img src="./images/icon-facebook.svg" alt="" style="height:50%; ">`
+    let fbs=document.getElementById("fbs");
+    fbs.innerHTML=""
+    let fbst=document.getElementById("fbst");
+    fbst.innerHTML=""
+}
+function updt(){
+    let tw=document.getElementById("tw");
+    tw.innerHTML=` <div class="card-header">
+            <img src="images/icon-twitter.svg" alt="tw-icon" />
+            <p>@ronaldosui</p>
+          </div>
+          <div class="card-body">
+            <h1>3450</h1>
+            <p>followers</p>
+          </div>
+          <div class="card-footer">
+            <img src="images/icon-up.svg" alt="up-icon" />
+            <p>87 today</p>
+          </div>`
+    let tws=document.getElementById("tws");
+    tws.innerHTML=` <h3 class="page-views">likes</h3>
+          <img class="fb-icon" src="images/icon-twitter.svg" alt="in-icon" />
+          <h2>5462</h2>
+          <img class="up-down-icon" src="images/icon-up.svg" alt="up" />
+          <p class="percentage in-up">2257%</p>`
+    let twst=document.getElementById("twst");
+    twst.innerHTML=` <h3 class="page-views">Retweets</h3>
+          <img class="fb-icon" src="images/icon-twitter.svg" alt="in-icon" />
+          <h2>5290</h2>
+          <img class="up-down-icon" src="images/icon-up.svg" alt="up" />
+          <p class="percentage in-up">1375%</p>`
+}
+function updat(){
+     let tw=document.getElementById("tw");
+    tw.innerHTML=`<img src="./images/icon-twitter.svg" alt="" style="height:50%; ">`
+     let tws=document.getElementById("tws");
+    tws.innerHTML=""
+    let twst=document.getElementById("twst");
+    twst.innerHTML=""
+}
+function updi(){
+    let ins=document.getElementById("ins");
+    ins.innerHTML=`  <div class="card-header">
+            <img src="images/icon-instagram.svg" alt="in-icon" />
+            <p>@ronaldosui</p>
+          </div>
+          <div class="card-body">
+            <h1>9K</h1>
+            <p>followers</p>
+          </div>
+          <div class="card-footer">
+            <img src="images/icon-up.svg" alt="up-icon" />
+            <p>2100 today</p>
+          </div>`
+    let int=document.getElementById("int");
+    int.innerHTML=` <h3 class="page-views">Story views</h3>
+          <img class="fb-icon" src="images/icon-instagram.svg" alt="tw-icon" />
+          <h2>11700</h2>
+          <img class="up-down-icon" src="images/icon-up.svg" alt="up" />
+          <p class="percentage tw-up">303%</p>`
+    let intt=document.getElementById("intt");
+    intt.innerHTML=`  <h3 class="page-views">likes</h3>
+          <img class="fb-icon" src="images/icon-instagram.svg" alt="tw-icon" />
+          <h2>50070</h2>
+          <img class="up-down-icon" src="images/icon-up.svg" alt="up" />
+          <p class="percentage tw-up">553%</p>`
+}
+function updai(){
+     let ins=document.getElementById("ins");
+    ins.innerHTML=`<img src="./images/icon-instagram.svg" alt="" style="height:50%; ">`
+    let int=document.getElementById("int");
+    int.innerHTML=""
+    let intt=document.getElementById("intt");
+    intt.innerHTML=""
+}
+function updy(){
+    let yb=document.getElementById("yb");
+    yb.innerHTML=`  <div class="card-header">
+            <img src="images/icon-youtube.svg" alt="yb-icon" />
+            <p>@ronaldosui</p>
+          </div>
+          <div class="card-body">
+            <h1>12K</h1>
+            <p>subscribers</p>
+          </div>
+          <div class="card-footer">
+            <img src="images/icon-down.svg" alt="down-icon" />
+            <p>4104 today</p>
+          </div>`
+    let ybs=document.getElementById("ybs");
+    ybs.innerHTML=` <h3 class="page-views">likes</h3>
+          <img class="fb-icon" src="images/icon-youtube.svg" alt="yb-icon" />
+          <h2>107</h2>
+          <img class="up-down-icon" src="images/icon-down.svg" alt="up" />
+          <p class="percentage yb-down">19%</p>`
+    let ybst=document.getElementById("ybst");
+    ybst.innerHTML=`   <h3 class="page-views">total views</h3>
+          <img class="fb-icon" src="images/icon-youtube.svg" alt="tb-icon" />
+          <h2>1407</h2>
+          <img class="up-down-icon" src="images/icon-down.svg" alt="up" />
+          <p class="percentage yb-down">12%</p>`
+}
+function upday(){
+     let yb=document.getElementById("yb");
+    yb.innerHTML=`<img src="./images/icon-youtube.svg" alt="" style="height:50%; ">`
+    let ybs=document.getElementById("ybs");
+    ybs.innerHTML=""
+    let ybst=document.getElementById("ybst");
+    ybst.innerHTML=""
+}
